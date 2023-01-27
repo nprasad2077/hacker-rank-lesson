bef990f8641b0e856492157895c2806a198c5c93
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-27 14:57:25 -0600

    plus minus challenge complete

---
+++ plusMinus/plusminus.js
@@ -0,0 +1,32 @@
+const arr = [-4, 3, -9, 0, 4, 1]
+const arrTwo = [-2]
+
+function plusMinus(arr) {
+    let positive = 0
+    let negative = 0
+    let zero = 0
+
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (element < 0) {
+            negative++
+        }
+        if ( element > 0){
+            positive++
+        }
+        if (element === 0) {
+            zero++
+        }
+        
+    }
+    let positiveRatio = (positive/(arr.length)).toFixed(6)
+    let negativeRatio = (negative/(arr.length)).toFixed(6)
+    let zeroRatio = (zero/(arr.length)).toFixed(6)
+
+    console.log(positiveRatio)
+    console.log(negativeRatio)
+    console.log(zeroRatio)
+    
+}
+
+plusMinus(arr);
