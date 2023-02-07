a53ffb44d6b96b738643c4547c279d9338bbbbb0
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-07 12:16:58 -0600

    sock pair solution python

--- sockMerchant/sockMerchant.py
+++ sockMerchant/sockMerchant.py
@@ -4,22 +4,18 @@ n = 7
 ar = [1, 2, 1, 2, 1, 3,2]
 
 def sockMerchant(n, ar):
-    bank = {4: 10}
+    bank = {}
     pairs = 0
     for i in ar:
         bank.update({i: 0})
-        print(bank)
-
     for i in ar:
         bank[i]+=1
-    
-    print(bank)
 
     for i in bank:
-        print(bank[i]/2)
         pairs += math.floor(bank[i]/2)
     
     print(pairs)
+    return pairs
 
 
 
