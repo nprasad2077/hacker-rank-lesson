6e3d297f1a379dd6a0f666f2c98484d46dd0e4c1
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-02-07 12:55:25 -0600

    zig zag init

--- sockMerchant/sockMerchant.py
+++ sockMerchant/sockMerchant.py
@@ -1,7 +1,8 @@
 import math, os, random, re, sys
 
 n = 7
-ar = [1, 2, 1, 2, 1, 3,2]
+ar = [1, 2, 1, 2, 1, 3, 2]
+
 
 def sockMerchant(n, ar):
     bank = {}
@@ -9,21 +10,13 @@ def sockMerchant(n, ar):
     for i in ar:
         bank.update({i: 0})
     for i in ar:
-        bank[i]+=1
+        bank[i] += 1
 
     for i in bank:
-        pairs += math.floor(bank[i]/2)
-    
+        pairs += math.floor(bank[i] / 2)
+
     print(pairs)
     return pairs
 
 
-
-
-
-
-
-
-
 sockMerchant(n, ar)
-
